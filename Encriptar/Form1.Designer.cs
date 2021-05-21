c2bf499d21012e248d7226d376d749f1f5b6f91f
Rasec <rasec.camacho19@gmail.com>
2021-05-21 12:31:59 -0700

    Primer commit

---
+++ Encriptar/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Encriptar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtImprimir = new System.Windows.Forms.TextBox();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnDesEncriptar = new System.Windows.Forms.Button();
+            this.btnArchivos_Images = new System.Windows.Forms.Button();
+            this.txtGuardar = new System.Windows.Forms.Button();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.btnImagen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtImprimir
+            // 
+            this.txtImprimir.Location = new System.Drawing.Point(36, 174);
+            this.txtImprimir.Multiline = true;
+            this.txtImprimir.Name = "txtImprimir";
+            this.txtImprimir.ReadOnly = true;
+            this.txtImprimir.Size = new System.Drawing.Size(264, 131);
+            this.txtImprimir.TabIndex = 1;
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(91, 74);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(152, 20);
+            this.txtDato.TabIndex = 2;
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(187, 136);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(75, 23);
+            this.btnEncriptar.TabIndex = 3;
+            this.btnEncriptar.Text = "Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.BtnEncriptar_Click);
+            // 
+            // btnDesEncriptar
+            // 
+            this.btnDesEncriptar.Location = new System.Drawing.Point(67, 136);
+            this.btnDesEncriptar.Name = "btnDesEncriptar";
+            this.btnDesEncriptar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesEncriptar.TabIndex = 4;
+            this.btnDesEncriptar.Text = "Desencriptar";
+            this.btnDesEncriptar.UseVisualStyleBackColor = true;
+            this.btnDesEncriptar.Click += new System.EventHandler(this.BtnDesEncriptar_Click);
+            // 
+            // btnArchivos_Images
+            // 
+            this.btnArchivos_Images.Location = new System.Drawing.Point(249, 74);
+            this.btnArchivos_Images.Name = "btnArchivos_Images";
+            this.btnArchivos_Images.Size = new System.Drawing.Size(31, 20);
+            this.btnArchivos_Images.TabIndex = 5;
+            this.btnArchivos_Images.Text = "...";
+            this.btnArchivos_Images.UseVisualStyleBackColor = true;
+            this.btnArchivos_Images.Click += new System.EventHandler(this.BtnArchivos_Images_Click);
+            // 
+            // txtGuardar
+            // 
+            this.txtGuardar.Location = new System.Drawing.Point(129, 100);
+            this.txtGuardar.Name = "txtGuardar";
+            this.txtGuardar.Size = new System.Drawing.Size(75, 23);
+            this.txtGuardar.TabIndex = 6;
+            this.txtGuardar.Text = "Guardar";
+            this.txtGuardar.UseVisualStyleBackColor = true;
+            this.txtGuardar.Click += new System.EventHandler(this.TxtGuardar_Click);
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.BackColor = System.Drawing.Color.Gainsboro;
+            this.pbImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbImagen.Location = new System.Drawing.Point(322, 12);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(283, 226);
+            this.pbImagen.TabIndex = 7;
+            this.pbImagen.TabStop = false;
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.Location = new System.Drawing.Point(420, 244);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(75, 23);
+            this.btnImagen.TabIndex = 8;
+            this.btnImagen.Text = "Imagen";
+            this.btnImagen.UseVisualStyleBackColor = true;
+            this.btnImagen.Click += new System.EventHandler(this.BtnImagen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(637, 332);
+            this.Controls.Add(this.btnImagen);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.txtGuardar);
+            this.Controls.Add(this.btnArchivos_Images);
+            this.Controls.Add(this.btnDesEncriptar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.txtImprimir);
+            this.Name = "Form1";
+            this.Text = "Encriptacion(Algoritmo DES)";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtImprimir;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnDesEncriptar;
+        private System.Windows.Forms.Button btnArchivos_Images;
+        private System.Windows.Forms.Button txtGuardar;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.Button btnImagen;
+    }
+}
+
---
+++ Encriptar/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Encriptar
+{
+    public partial class Form1 : Form
+    {
+        public static string key = "abc123ky";
+        public string Encriptar(string dato, string key) {
+            byte[] keyArray = Encoding.UTF8.GetBytes(key);
+            byte[] encriptar = Encoding.UTF8.GetBytes(dato);
+
+            var DES = new DESCryptoServiceProvider();
+            DES.Key = keyArray;
+            DES.Mode = CipherMode.ECB;
+            DES.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = DES.CreateEncryptor();
+            byte[] resultado = cTransform.TransformFinalBlock(encriptar, 0, encriptar.Length);
+            DES.Clear();
+
+            return Convert.ToBase64String(resultado, 0, resultado.Length);
+
+        }
+
+        public string DesEncriptar(string dato, string key)
+        {
+            byte[] keyArray = Encoding.UTF8.GetBytes(key);
+            byte[] desEncriptar = Convert.FromBase64String(dato);
+
+            var DES = new DESCryptoServiceProvider();
+            DES.Key = keyArray;
+            DES.Mode = CipherMode.ECB;
+            DES.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = DES.CreateDecryptor();
+            byte[] resultado = cTransform.TransformFinalBlock(desEncriptar, 0, desEncriptar.Length);
+            DES.Clear();
+
+            return Encoding.UTF8.GetString(resultado);
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEncriptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtImprimir.Text = Encriptar(txtDato.Text, key);
+            }
+            catch (Exception) {
+                MessageBox.Show("Escriba codigo valido para cifrar", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void BtnDesEncriptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtImprimir.Text = DesEncriptar(txtDato.Text, key);
+            }
+            catch (Exception) {
+                MessageBox.Show("Escriba codigo valido para decifrar","Alerta",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void BtnArchivos_Images_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ventanaEmergente = new OpenFileDialog();
+                ventanaEmergente.Filter = "Archivos de texto|*.txt";
+
+                if (ventanaEmergente.ShowDialog() == DialogResult.OK)
+                {
+
+                    using (StreamReader leer = new StreamReader(ventanaEmergente.FileName))
+                    {
+
+                        txtDato.Text = leer.ReadToEnd();
+                    }
+
+                }
+            }
+            catch (Exception) { }
+
+
+        }
+
+        private void TxtGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog ventanaEmergente = new SaveFileDialog();
+                ventanaEmergente.Filter = "Archivos de texto|*.txt";
+
+                if (ventanaEmergente.ShowDialog() == DialogResult.OK)
+                {
+
+                    using (StreamWriter escribir = new StreamWriter(ventanaEmergente.FileName))
+                    {
+
+                        escribir.Write(txtImprimir.Text);
+                    }
+
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void BtnImagen_Click(object sender, EventArgs e)
+        {
+            try{
+                OpenFileDialog ventanaEmergente = new OpenFileDialog();
+                ventanaEmergente.Filter = "Archivos de imagen|*.jpg";
+
+                if (ventanaEmergente.ShowDialog() == DialogResult.OK)
+                {
+                    Image img = Image.FromFile(ventanaEmergente.FileName);
+
+                    pbImagen.Image = img;
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
